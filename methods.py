4c26f210a4924443d8552151fbeab7f8cb530a6f
anoopbaragi <anoop.baragi@gmail.com>
2022-12-26 15:26:44 +0530

    Phython

---
+++ methods.py
@@ -0,0 +1,45 @@
+# a=5
+# b=10
+# # print(a+b)
+
+#methods
+# def add(a,b):
+#     print(a+b)
+# add(5,10)
+
+
+# def it(str):
+#     for x in str:
+#         print(x)
+# it("Anoop")
+
+#method overloading
+
+# def a(str):
+#     print(str)
+# a("Hello")
+# def a(str1):
+#     print(str1)
+# a("Anoop")
+
+# def c(a,b):
+#     print(a+b)
+# c(5, 10)
+# def c():
+#     print("Method overloading")
+# c()
+
+#return statement
+
+def add(a,b,c):
+    '''
+
+    :param a:
+    :param b:
+    :param c:
+    :return:
+    '''
+    return a+b+c
+z = add(10,20,30)
+
+print(z)
--- rangefunction.py
+++ rangefunction.py
@@ -1,3 +1,20 @@
-a = [1,2,3,4,5,6,7,8,9,10]
-for x in a:
-    print(x)
+# a = [1,2,3,4,5,6,7,8,9,10]
+# for x in a:
+#     print(x)
+# for x in range(1,11):
+#     print(x)
+# even and odd
+for x in range(1,10):
+    if(x%2 == 0):
+        print(x, "Even")
+    else:
+        print(x,"Odd")
+#table of 23
+for y in range(1,11):
+    print("23 * " ,y, " = ", 23*y)
+#even and odd using only range not condition satement
+for z in range(1,10,2):
+    print(z,end=' ')
+print("\n")
+for c in range(2,10,2):
+    print(c,end=' ')
